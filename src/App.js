0de4a045193d35d5688046a3a4a8b28500e3d0f9
sarangan16 <mailsarangan@gmail.com>
2026-04-24 12:04:36 +0200

    initial forced loader has been removed, and unmounted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import "./App.css";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
@@ -7,13 +7,10 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import "./components/i18n";
 import { useTranslation } from "react-i18next";
-import Loader from "./components/Loader";
 
 function App() {
   const { i18n } = useTranslation();
-  const [loading, setLoading] = useState(true);
 
-  // SECTION REFS (THIS FIXES EVERYTHING)
   const heroRef = useRef(null);
   const projectsRef = useRef(null);
   const stackRef = useRef(null);
@@ -25,40 +22,34 @@ function App() {
 
   return (
     <div className="bg-black text-white">
-      {loading ? (
-        <Loader onComplete={() => setLoading(false)} />
-      ) : (
-        <>
-          <Navbar
-            heroRef={heroRef}
-            projectsRef={projectsRef}
-            stackRef={stackRef}
-            contactRef={contactRef}
-          />
+      <Navbar
+        heroRef={heroRef}
+        projectsRef={projectsRef}
+        stackRef={stackRef}
+        contactRef={contactRef}
+      />
 
-          <main>
-            {/* HERO */}
-            <section ref={heroRef}>
-              <Hero projectsRef={projectsRef} />
-            </section>
+      <main>
+        {/* HERO */}
+        <section ref={heroRef}>
+          <Hero projectsRef={projectsRef} />
+        </section>
 
-            {/* PROJECTS */}
-            <section ref={projectsRef}>
-              <Projects />
-            </section>
+        {/* PROJECTS */}
+        <section ref={projectsRef}>
+          <Projects />
+        </section>
 
-            {/* STACK */}
-            <section ref={stackRef}>
-              <TechStack />
-            </section>
+        {/* STACK */}
+        <section ref={stackRef}>
+          <TechStack />
+        </section>
 
-            {/* CONTACT */}
-            <section ref={contactRef}>
-              <Contact />
-            </section>
-          </main>
-        </>
-      )}
+        {/* CONTACT */}
+        <section ref={contactRef}>
+          <Contact />
+        </section>
+      </main>
     </div>
   );
 }
